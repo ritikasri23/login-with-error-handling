e818850c9c9bd73d055537dc83903033b7df566a
Ritika Srivastava <ritika86.srivastava@gmail.com>
2018-01-30 11:44:38 +0100

    Initial Commit

---
+++ DoCatchTryExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoCatchTryExample
+//
+//  Created by Ritika Srivastava on 30/01/18.
+//  Copyright © 2018 Ritika Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
